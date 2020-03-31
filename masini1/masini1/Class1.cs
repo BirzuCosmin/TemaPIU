d936d6c0a287f11a3834e047cbeb6165218e4e20
BirzuCosmin <62953830+BirzuCosmin@users.noreply.github.com>
2020-03-31 19:36:36 +0300

    Add files via upload

---
+++ masini1/masini1/Class1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+namespace masini
+{
+    
+    class Class1
+    {
+        
+        static void Main(string[]args)
+        { 
+            masina[] vanzari=new masina[300];
+             int nrvanzari=0;
+            
+           
+        //IStocareData adminMasini = Stocare;
+        //IStocareData adminMasini= S  
+
+        // masina.IdUltimaVanare = nrmasini;
+        var m = new masina();
+            string s = m.info();
+            Console.WriteLine(s);
+
+            masina m2 = new masina("cosmin", "alin", "Audi", "A4");
+            string s1 = m2.info();
+            Console.WriteLine(s1);
+            masina m3 = new masina("Alex Matei BMW m3");
+            string s2 = m2.ConversieLaSir();
+            Console.WriteLine(m3.info());
+            Console.WriteLine(s2);   
+            if (m3.Comparatie(m2) == true)
+            {
+                Console.WriteLine("s-a introdus aceeasi vanzare de 2 ori");
+            }
+            else
+                Console.WriteLine("vanzarile sunt diferite");
+            string optiune;
+             do
+             {
+                 Console.WriteLine("L.  lista vanzari");
+                 Console.WriteLine("C.  adauga masina");
+                 Console.WriteLine("S.  salvare fisier");
+                Console.WriteLine("P.  afisare date fisier");
+                 Console.WriteLine("X.  Inchidere program");
+                 Console.WriteLine("alegeti o optiune");
+                 optiune = Console.ReadLine();
+                 switch (optiune.ToUpper())
+                 {
+  
+                     case "C":
+                        var k = new masina();
+                          k  = CitireTastatura();
+                         vanzari[nrvanzari] = k;
+                        nrvanzari++;
+ 
+                        break;
+                    case "L":
+                        AfisareMasini(vanzari, nrvanzari);
+                         break;  
+                    case "S":
+                        using (StreamWriter sr= new StreamWriter("vanzari.txt", true))
+                        {
+                            for (int i=0;i<nrvanzari;i++)
+                            {   
+                                sr.WriteLine(vanzari[i].ConversieLaSir());
+                            }
+                        }
+                        break;
+                    case "P":
+                        using (StreamReader sw = new StreamReader("vanzari.txt", true))
+                        {
+                            string linie;
+                            nrvanzari = 0;
+                            while((linie=sw.ReadLine())!=null)
+                            {
+                                vanzari[nrvanzari++] = new masina(linie);
+                            }
+                            
+                        }
+                        for (int i =0 ; i <nrvanzari; i++)
+                        {
+                            Console.WriteLine(vanzari[i].ConversieLaSir());
+                        }
+                        break;
+                    case "V":
+                        Console.WriteLine("itroduceti numele vanzatorului pe care doriti sa il schimbati");
+                        string NumeV = Console.ReadLine();
+                        Console.WriteLine("introduceti nume nou al vanzatorului");
+                        string numenou = Console.ReadLine();
+                        cautareVanzator(vanzari, NumeV,numenou);
+                        
+
+                        
+                            break;
+                        
+                 }
+             } while (optiune.ToUpper() != "X");
+            Console.ReadKey();
+        }
+         public static masina CitireTastatura()
+         {
+             Console.WriteLine("introduceti nume vanzator");
+             string numev = Console.ReadLine();
+             Console.WriteLine("introduceti nume cumparator");
+             string numec = Console.ReadLine();
+             Console.WriteLine("introduceti firma masinii");
+             string numef = Console.ReadLine();
+             Console.WriteLine("introduceti modelul masinii");
+             string numem = Console.ReadLine();
+             masina m = new masina(numev, numec, numef, numem);
+             return m;
+         }
+         public static void AfisareMasini(masina[] vanzari,int NrMasini)
+         {
+             Console.WriteLine("vanzarile sunt: ");
+             for(int i=0; i<NrMasini;i++)
+             {
+                 Console.WriteLine( vanzari[i].ConversieLaSir());
+                Console.ReadKey();
+             }
+         }
+         public  static void cautareVanzator(masina[] vanzari,string numev,string numenou)
+        { 
+
+            int nrvanzari = 0;
+            using (StreamReader sw = new StreamReader("vanzari.txt", true))
+            {
+                string linie;
+               
+                while ((linie = sw.ReadLine()) != null)
+                {
+                    vanzari[nrvanzari++] = new masina(linie);
+                }
+            }
+            for (int i = 0; i < nrvanzari; i++)
+            {
+                if(vanzari[i].comp(numev)==true)
+                {
+                    vanzari[i].set(numenou);
+                }
+                    
+            }
+        }
+    }
+}
---
+++ masini1/masini1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace masini
+{
+    class masina
+    {
+
+        string nume_vanzator;
+       private  string nume_cumparator;
+        private string firma;
+       private  string model;
+        public static int IdUltimaVanare { get; set; } = 0;
+     
+        public enum culoare
+        {
+            rosu = 1,
+            albastru = 2,
+            alb = 3,
+            negru = 4,
+            gri = 5,
+            verde = 6
+        };
+        public enum dotari
+        {
+            aer_conditionat = 1,
+            navigatie = 2,
+            senzori_parcare = 3,
+            cruise_control = 4,
+        };
+        public masina()
+        {
+            nume_vanzator = string.Empty;
+            nume_cumparator = string.Empty;
+            firma = string.Empty;
+            model = string.Empty;
+        }
+        public masina(string nume_v, string nume_c, string _firma, string _model)
+        {
+            nume_vanzator = nume_v;
+            nume_cumparator = nume_c;
+            firma = _firma;
+            model = _model;
+        }
+        public masina(string date)
+        {
+            string[] data = date.Split(" ");
+            nume_vanzator = data[0];
+            nume_cumparator = data[1];
+            firma = data[2];
+            model = data[3];
+
+        }
+        public string ConversieLaSir()
+        {
+            string sir;
+            sir = string.Join(" ",nume_vanzator, nume_cumparator, firma, model);
+            return sir;
+        }
+
+        public string info()
+        {
+            return string.Format("vanzatorul este {0}, cumparatorul este {1}, firma este {2}, modelul este {3}", nume_vanzator, nume_cumparator, firma, model);
+        }
+        public bool Comparatie (masina a)
+        {
+            if (a.nume_cumparator == nume_cumparator && a.nume_vanzator == nume_vanzator && a.firma ==firma && a.model == model)
+                return true;
+            else
+                return false;
+                
+        }
+        public bool comp( string nume)
+        { if (nume_vanzator == nume)
+                return true;
+            else
+                return false;
+
+        
+        }
+        public string set(string Numev)
+        {
+            nume_cumparator = Numev;
+            return null;
+        }
+        
+    }
+}
